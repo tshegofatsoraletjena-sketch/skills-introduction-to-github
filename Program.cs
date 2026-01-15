f5b2794264d1e7119d76f7398be22c85ac739ed5
tshegofatsoraletjena-sketch <tshegofatso.raletjena@ul.ac.za>
2026-01-15 05:06:49 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //printout
+            Console.WriteLine("Hello, World!");
+
+            int number1;
+            int number2;
+            int results;
+
+            //hard coded values
+            number2 = 10;
+            number1 = 15;
+
+            results = number1 + number2;
+            Console.WriteLine(results);
+            double name;
+            Boolean isJavaFun;
+            char symbol;
+            string age;
+            age = 25;
+            height =5,9;
+            iStudent = true;
+            grade ='A';
+            name = "John Dean";
+
+            //Institite and declare is one line
+            int score = 90;
+            int total = age + score;
+            Console.WriteLine(score);
+            Console.WriteLine(age);// Added missing semicolon
+            Console.WriteLine(total)
+        }
+    }
+}
